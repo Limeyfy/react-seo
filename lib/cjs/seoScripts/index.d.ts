36861cfc4640486329333e8ae23b8ac99531778c
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-03-28 12:24:37 +0200

    Removed lib folder & fixed useSeo with percentage

--- lib/cjs/seoScripts/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IResult } from "../useSeo";
-declare const CheckTitles: () => IResult;
-declare const CheckPageTitle: () => IResult;
-declare const CheckDescription: () => IResult;
-declare const CheckKeywords: () => IResult;
-declare const CheckImages: () => IResult;
-declare const CheckForNoIndexTag: () => IResult;
-export { CheckTitles, CheckPageTitle, CheckDescription, CheckKeywords, CheckImages, CheckForNoIndexTag };
--- src/useSeo.tsx
+++ src/useSeo.tsx
@@ -12,19 +12,49 @@ interface ResultProps {
     keywords: IResult;
     images: IResult;
     noIndexTag: IResult;
+    percentage: number;
+    checked: number
 }
 
+interface SimpleResult {
+    title: IResult;
+    header: IResult;
+    description: IResult;
+    keywords: IResult;
+    images: IResult;
+    noIndexTag: IResult;
+}
+
+const calculatePercentage = (num1: number, num2: number) => (100 * num2) / num1;
 
 const useSeo = () => {
+    const getResultPercentage = (props: SimpleResult) => {
+        let total = Object.keys(props).length;
+        let checked = 0;
+        Object.keys(props).map(prop => {
+            if (props[prop].checked) checked++;
+            return;
+        })
+        return {
+            percentage: calculatePercentage(total, checked),
+            checked
+        }
+    }
 
     const result = () => {
-        var res: ResultProps = {
+        var simpleRes: SimpleResult = {
             title: CheckTitles(),
             header: CheckPageTitle(),
             description: CheckDescription(),
             images: CheckImages(),
             keywords: CheckKeywords(),
             noIndexTag: CheckForNoIndexTag()
+        }
+        var resPercentage = getResultPercentage(simpleRes);
+        var res: ResultProps = {
+            ...simpleRes,
+            checked: resPercentage.checked,
+            percentage: resPercentage.percentage
         }
         return res;
     }
